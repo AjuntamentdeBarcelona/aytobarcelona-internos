6866e48ef46e84c4fa86c9c94afe859727b68eeb
Oliver Valls <oliver.vh@coditramuntana.com>
2019-11-18 13:51:48 +0100

    Add initializer to modify Length validator for Proposal.title.

---
+++ config/initializers/proposal_wizard_create_step_form_with_extended_title_size.rb
@@ -0,0 +1,8 @@
+# extends the title size to 250 characters
+Rails.application.configure do
+  config.after_initialize do
+   Decidim::Proposals::ProposalWizardCreateStepForm
+   .validators_on(:title).find{|v|v.is_a?(ActiveModel::Validations::LengthValidator)}
+   .instance_variable_set(:@options, {maximum:250})
+  end
+end
