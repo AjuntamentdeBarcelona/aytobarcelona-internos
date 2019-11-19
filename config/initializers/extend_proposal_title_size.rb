1b4a8f67cdcdcc5004372d0f8054f5315703d7e2
Oliver Valls <oliver.vh@coditramuntana.com>
2019-11-19 15:13:38 +0100

    [REFACTOR] Apply maximum of 250 char for Proposal.title in the Admin.

--- config/initializers/extend_proposal_title_size.rb
+++ config/initializers/extend_proposal_title_size.rb
@@ -4,5 +4,8 @@ Rails.application.configure do
    Decidim::Proposals::ProposalWizardCreateStepForm
    .validators_on(:title).find{|v|v.is_a?(ActiveModel::Validations::LengthValidator)}
    .instance_variable_set(:@options, {maximum:250})
+   Decidim::Proposals::Admin::ProposalForm
+   .validators_on(:title).find{|v|v.is_a?(ActiveModel::Validations::LengthValidator)}
+   .instance_variable_set(:@options, {maximum:250})
   end
 end
