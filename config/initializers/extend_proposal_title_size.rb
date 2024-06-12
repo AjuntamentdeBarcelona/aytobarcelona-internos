dbf862852e63afa4d590748f8378070fe8b9f93e
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-06-12 15:44:50 +0200

    Fix extend proposal title size initializer

--- config/initializers/extend_proposal_title_size.rb
+++ config/initializers/extend_proposal_title_size.rb
@@ -4,7 +4,7 @@
 Rails.application.configure do
   config.after_initialize do
     Decidim::Proposals::ProposalWizardCreateStepForm
-      .validators_on(:title).find { |v| v.is_a?(ActiveModel::Validations::LengthValidator) }
+      .validators_on(:title).find { |v| v.is_a?(ProposalLengthValidator) }
       .instance_variable_set(:@options, { maximum: 250 })
     Decidim.available_locales.each do |locale|
       Decidim::Proposals::Admin::ProposalForm
