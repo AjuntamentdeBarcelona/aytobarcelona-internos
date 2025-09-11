52489c3f7541d449452b8a6ac6a621235e544827
Fran Bolívar <francisco.bolivar@nazaries.com>
2025-09-11 16:55:51 +0200

    Bump decidim from 0.29.4 to 0.30.1

--- db/migrate/20181214111761_add_configuration_to_features.decidim.rb
+++ db/migrate/20181214111761_add_configuration_to_features.decidim.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
-# This migration comes from decidim (originally 20170110133113)
 
+# This migration comes from decidim (originally 20170110133113)
+# This file has been modified by `decidim upgrade:migrations` task on 2025-09-11 14:52:46 UTC
 class AddConfigurationToFeatures < ActiveRecord::Migration[5.0]
   def change
     change_table :decidim_features do |t|
