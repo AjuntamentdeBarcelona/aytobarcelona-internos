5f3c5cc51b45d19c984ffa2682d27c8b76561e0a
Alejandro Rueda López (ARU) <alejandro.rueda@nazaries.com>
2025-11-18 09:14:17 +0100

    Display user alias on mouse hover at proposal endorsers list (#88)
    * Display user alias on mouse hover at proposal endorsers list
    
    * Include test
    
    * Lint
    
    * Include link to user profile

---
+++ spec/cells/decidim/endorsers_list/endorsers_list_cell_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "decidim/proposals/test/factories"
+
+module Decidim
+  describe EndorsersListCell, type: :cell do
+    controller Decidim::PagesController
+
+    let(:organization) { create(:organization) }
+    let(:user) { create(:user, :confirmed, organization: organization) }
+    let(:other_user) { create(:user, :confirmed, nickname: "test_user", organization: organization) }
+    let(:participatory_space) { create(:participatory_process, organization: organization) }
+    let(:component) { create(:proposal_component, participatory_space: participatory_space) }
+    let(:proposal) { create(:proposal, component: component) }
+    let!(:endorsement) { create(:endorsement, resource: proposal, author: other_user) }
+
+    context "when rendering the endorsers list modal" do
+      subject { cell("decidim/endorsers_list", proposal, layout: :full).call }
+
+      before do
+        allow(controller).to receive(:current_user).and_return(user)
+        allow(controller).to receive(:current_organization).and_return(organization)
+      end
+
+      it "displays the user nickname in the title attribute on hover" do
+        expect(subject).to have_css("div[title='@test_user']")
+      end
+
+      it "includes a link to the user profile" do
+        expect(subject).to have_link(other_user.name, href: "/profiles/test_user")
+      end
+    end
+  end
+end
